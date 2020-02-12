c83d7bde02fc4d51c586149c2b49810e7d564a45
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-12 15:15:28 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(argument)
+  if block_given?
+    n = 0
+    while n < argument.length
+      string = argument[n]
+      yield string
+      n += 1
+    end
+    argument
+  else
+    return "No block was given"
+  end
 end
